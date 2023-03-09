1ed2d314cc3a72f77360e71bad0aa1b700d6c7f4
Matheus-Uchoa/ <matheusuchoa@unitins.br>
2023-03-08 22:33:41 -0300

    Adicionando lógica do conversor

--- src/br/alura/desafio/service/ConversorMoedas.java
+++ src/br/alura/desafio/service/ConversorMoedas.java
@@ -1,5 +1,7 @@
 package br.alura.desafio.service;
 
+import javax.swing.JComboBox;
+
 public class ConversorMoedas {
 	private double valor;
 
@@ -19,6 +21,55 @@ public class ConversorMoedas {
 
 	public String[] getOpcoesConversao() {
 		return opcoesConversao;
+	}
+
+	public String selecionaopcao(JComboBox<String> caixa, Double valor) {
+		switch (caixa.getSelectedItem().toString()) {
+		case "De Reais a Dólares":
+			valor *= 0.19;
+
+			return "$" + valor.toString();
+
+		case "De Reais a Euros":
+			valor *= 0.18;
+			return "€" + valor.toString();
+
+		case "De Reais a Libras":
+			valor *= 0.16;
+			return "£" + valor.toString();
+
+		case "De Reais a Yenes":
+			valor *= 26.64;
+			return "¥" + valor.toString();
+
+		case "De Reais a Won Coreano":
+			valor *= 256.22;
+			return "₩" + valor.toString();
+
+		case "De Dólares a Reais":
+			valor *= 5.15;
+			return "R$" + valor.toString();
+
+		case "De Euros a Reais":
+			valor *= 5.46;
+			return "R$" + valor.toString();
+
+		case "De Libras a Reais":
+			valor *= 6.09;
+			return "R$" + valor.toString();
+
+		case "De Yenes a Reais":
+			valor *= 0.038;
+			return "R$" + valor.toString();
+
+		case "De Won Coreano a Reais":
+			valor *= 0.0039;
+			return "R$" + valor.toString();
+
+		default:
+			return "ERROR";
+
+		}
 	}
 
 }
